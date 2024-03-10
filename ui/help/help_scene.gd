3f606ef06887d70b90b90ace947df99c733428a7
voice of texnoforge <voice@texnoforge.dev>
2024-03-10 17:06:42 +0100

    Make help links actually work

--- ui/help/help_scene.gd
+++ ui/help/help_scene.gd
@@ -3,3 +3,10 @@ extends Control
 
 func _on_Button_pressed():
 	self.visible = false
+
+
+func _on_Content_meta_clicked(meta):
+	if OS.has_feature('JavaScript'):
+		JavaScript.eval("""window.open('%s', '_blank').focus();""" % meta)
+	else:
+		OS.shell_open(meta)
